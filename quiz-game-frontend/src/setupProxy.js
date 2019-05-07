d12f98b74a04ce676d2c8c0f4edc221a9521aab6
Ken Rimple <krimple@chariotsolutions.com>
2019-05-07 15:04:34 -0400

    removing proxy as we'll use cors

--- quiz-game-frontend/src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use('/graphql', proxy({ target: 'http://localhost', logLevel: 'debug', changeOrigin: true, ws: true}));
-};
